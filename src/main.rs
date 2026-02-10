b64e5788916f9b31535cdfe390e5b7257b39fc91
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-09 20:07:46 -0500

    Refactor error printing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use colored::Colorize;
 
 use crate::{
     lexer::{Severity, SourceCode},
-    parser::{Traverse, program},
+    parser::{LocatedSyntaxError, Traverse, program},
 };
 
 pub mod lexer;
@@ -13,35 +13,13 @@ pub mod parser;
 fn main() {
     let args = env::args().collect::<Vec<_>>();
     if args.len() == 1 {
-        println!("Syntax: {} <source-files>", args[0]);
+        println!("{} {} <source-file>", "Usage error:".bold().red(), args[0]);
         exit(1);
     }
 
     let source = SourceCode::read(args[1].as_str()).unwrap();
     let mut tokens = source.token_stream();
     let result = program(&mut tokens);
-    if let Ok(program) = result.as_ref() {
-        program.traverse(&|message, span| {
-            if source.is_single_line(span) {
-                source.print_token_span(span, '-', message, Severity::Note);
-            }
-        });
-        println!();
-    }
-    if let Err(syntax_err) = result {
-        tokens.errors.push(syntax_err);
-    }
-
-    let error_count = tokens.errors.len() + source.tokenizer_errors.len();
-    println!(
-        "Found {} {}\n",
-        error_count.to_string().cyan().red(),
-        if tokens.errors.len() == 1 {
-            "error"
-        } else {
-            "errors"
-        },
-    );
 
     for error in source.tokenizer_errors.iter() {
         println!("{} unexpected token", "Lexer error:".red().bold());
@@ -49,19 +27,36 @@ fn main() {
         println!();
     }
 
-    for error in tokens.errors {
-        print!(
-            "{} {}",
-            "Syntax error:".red().bold(),
-            error.message(source.tokens.clone())
-        );
-        println!();
-        source.print_token_span(
-            error.span,
-            '^',
-            error.inline_message().to_string().as_str(),
-            Severity::Error,
-        );
-        println!();
+    for error in tokens.errors.iter() {
+        print_err(&source, error);
     }
+
+    match result {
+        Ok(program) => {
+            program.traverse(&|message, span| {
+                if source.is_single_line(span) {
+                    source.print_token_span(span, '-', message, Severity::Note);
+                }
+            });
+        }
+        Err(error) => {
+            print_err(&source, &error);
+        }
+    }
+}
+
+fn print_err(source: &SourceCode, error: &LocatedSyntaxError) {
+    print!(
+        "{} {}",
+        "Syntax error:".red().bold(),
+        error.message(source.tokens.clone())
+    );
+    println!();
+    source.print_token_span(
+        error.span,
+        '^',
+        error.inline_message().to_string().as_str(),
+        Severity::Error,
+    );
+    println!();
 }
