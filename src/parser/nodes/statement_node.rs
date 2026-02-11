e4de4a2f7ab7d6487ee72c886330b608920dd2cf
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-11 14:09:37 -0500

    more statement checking

--- src/parser/nodes/statement_node.rs
+++ src/parser/nodes/statement_node.rs
@@ -17,12 +17,25 @@ pub enum StatementNode {
 impl StatementNode {
     pub fn check(&self, types: &TypeResolver, scope: Box<Scope>) -> (Box<Scope>, Option<Type>) {
         match self {
-            Self::BlockReturn(_node) => todo!("Implement type checking for `BlockReturn`"),
+            Self::BlockReturn(node) => {
+                // TODO check that this type matches the expected expression return type
+                let (scope, resolved_type) = node.check(types, scope);
+                (scope, Some(resolved_type))
+            }
             Self::Break() => todo!("Implement type checking for `Break`"),
             Self::Continue() => todo!("Implement type checking for `Continue`"),
             Self::Declaration(node) => (node.check(types, scope), None),
-            Self::Expression(_node) => todo!("Implement type checking for `Expression`"),
-            Self::FunctionReturn(_node) => todo!("Implement type checking for `FunctionReturn`"),
+            Self::Expression(node) => {
+                // Discard the type of raw expressions
+                let (scope, _) = node.check(types, scope);
+                (scope, None)
+            }
+            Self::FunctionReturn(Some(return_type)) => {
+                // TODO check return type
+                let (scope, _) = return_type.check(types, scope);
+                (scope, None)
+            }
+            Self::FunctionReturn(None) => (scope, None),
             Self::If(_node) => todo!("Implement type checking for `If`"),
             Self::WhileLoop(_node) => todo!("Implement type checking for `WhileLoop`"),
         }
