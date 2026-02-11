590dde631474fc99aa3c69f3394b23c2a9e7b775
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-10 20:54:45 -0500

    Derefs for parse node

--- src/parser/nodes/parse_node.rs
+++ src/parser/nodes/parse_node.rs
@@ -1,8 +1,24 @@
+use std::ops::{Deref, DerefMut};
+
 use crate::parser::TokenStream;
 
 pub struct ParseNode<T> {
     pub value: T,
     pub span: TokenSpan,
+}
+
+impl<T> Deref for ParseNode<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl<T> DerefMut for ParseNode<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
 }
 
 #[derive(Clone, Copy)]
