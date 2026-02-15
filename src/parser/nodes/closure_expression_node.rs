b307251dd0e6e6e8ff04a608f5e6fc2ae5a6e7b0
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-14 20:27:59 -0500

    Simplify closure inference

--- src/parser/nodes/closure_expression_node.rs
+++ src/parser/nodes/closure_expression_node.rs
@@ -15,53 +15,58 @@ impl ClosureExpressionNode {
         scope: Box<Scope>,
         expected_type: Option<&Type>,
     ) -> (Box<Scope>, Type) {
-        let function_type =
-            expected_type.and_then(|expected_type| get_function_type(expected_type, types));
-        if let Some(function_type) = function_type {
-            self.check_with_expected(types, scope, &function_type)
-        } else {
-            self.check_raw(types, scope)
-        }
-    }
-
-    fn check_with_expected(
-        &self,
-        _types: &TypeResolver,
-        _scope: Box<Scope>,
-        _expected_type: &FunctionType,
-    ) -> (Box<Scope>, Type) {
-        todo!("Implement type checking with inference");
-        // (scope, Type::Error)
-    }
-
-    fn check_raw(&self, types: &TypeResolver, scope: Box<Scope>) -> (Box<Scope>, Type) {
-        let mut scope = scope.derive(ScopeType::Closure);
-        let mut parameter_types = vec![];
-        for parameter in self.parameters.iter() {
-            if let Some(parameter) = parameter {
-                let parameter_type = if let Some(parameter_type) = parameter.parameter_type.as_ref()
-                {
-                    parameter_type.get_type(types)
-                } else {
-                    println!(
-                        "Type error: Could not infer type of closure parameter `{}`",
-                        parameter.id()
-                    );
-                    Type::Error
-                };
-                parameter_types.push(parameter_type.clone());
-                scope.add(parameter.id(), parameter_type);
-            } else {
-                parameter_types.push(Type::Error);
-            }
-        }
-
+        let function_type = expected_type.and_then(|t| get_function_type(t, types));
+        let scope = scope.derive(ScopeType::Closure);
+        let (scope, parameters) = self.check_parameters(function_type.as_ref(), types, scope);
         let (scope, return_type) = self.body.check(types, scope, None);
         let result_type = Type::Function(FunctionType {
-            parameters: parameter_types,
+            parameters,
             return_type: Box::new(return_type),
         });
-
         (scope.parent(), result_type)
+    }
+
+    fn check_parameters(
+        &self,
+        expected_type: Option<&FunctionType>,
+        types: &TypeResolver,
+        mut scope: Box<Scope>,
+    ) -> (Box<Scope>, Vec<Type>) {
+        let parameter_types = self
+            .parameters
+            .iter()
+            .enumerate()
+            .map(|(index, parameter)| {
+                if let Some(parameter) = parameter {
+                    let parameter_type = get_parameter_type(parameter, index, expected_type, types);
+                    scope.add(parameter.id(), parameter_type.clone());
+                    parameter_type
+                } else {
+                    Type::Error
+                }
+            })
+            .collect::<Vec<_>>();
+
+        (scope, parameter_types)
+    }
+}
+
+fn get_parameter_type(
+    parameter: &Node<ClosureParameterExpressionNode>,
+    index: usize,
+    expected_type: Option<&FunctionType>,
+    types: &TypeResolver,
+) -> Type {
+    let expected_type = expected_type.and_then(|ft| ft.parameters.get(index));
+    if let Some(given_type) = parameter.parameter_type.as_ref() {
+        given_type.get_type(types)
+    } else if let Some(expected_type) = expected_type {
+        expected_type.clone()
+    } else {
+        println!(
+            "Type error: Could not infer type of closure parameter `{}`",
+            parameter.id()
+        );
+        Type::Error
     }
 }
