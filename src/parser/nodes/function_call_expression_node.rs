013cc14f3c8b89f617f996e3e977a484589b7902
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-25 23:24:45 -0500

    Refactor function call expression node

--- src/parser/nodes/function_call_expression_node.rs
+++ src/parser/nodes/function_call_expression_node.rs
@@ -1,5 +1,5 @@
 use crate::{
-    checker::{FunctionType, Scope, Type, TypeResolver},
+    checker::{FunctionType, Scope, Type},
     parser::{ExpressionNode, Node, NodeVec},
 };
 
@@ -12,66 +12,56 @@ impl FunctionCallExpressionNode {
     pub fn check(&self, scope: Box<Scope>, expected_type: Option<&Type>) -> (Box<Scope>, Type) {
         let (scope, function_type) = self.function.check_expected(scope, expected_type);
         let function_type = function_type.as_function(&scope.types);
-        let (scope, arguments) = self.get_args(scope, function_type.as_ref());
+        check_function_call(scope, function_type.as_ref(), &self.arguments)
+    }
+}
 
-        if let Some(function_type) = function_type {
-            let result_type = self.check_args(function_type, &arguments, &scope.types);
-            (scope, result_type)
-        } else {
-            (scope, Type::Error)
-        }
+pub fn check_function_call(
+    mut scope: Box<Scope>,
+    function_type: Option<&FunctionType>,
+    argument_expressions: &Vec<Node<ExpressionNode>>,
+) -> (Box<Scope>, Type) {
+    let mut arguments = vec![];
+    for (index, argument) in argument_expressions.iter().enumerate() {
+        let parameter_type = function_type.and_then(|ft| ft.parameters.get(index));
+        let (new_scope, resolved_type) = argument.check_expected(scope, parameter_type);
+        arguments.push(resolved_type);
+        scope = new_scope;
     }
 
-    fn get_args(
-        &self,
-        mut scope: Box<Scope>,
-        function_type: Option<&FunctionType>,
-    ) -> (Box<Scope>, Vec<Type>) {
-        let mut result = vec![];
-        for (index, argument) in self.arguments.iter().enumerate() {
-            let parameter_type = function_type.and_then(|ft| ft.parameters.get(index));
-            let (new_scope, resolved_type) = argument.check_expected(scope, parameter_type);
-            result.push(resolved_type);
-            scope = new_scope;
-        }
-        (scope, result)
+    if function_type.is_none() {
+        return (scope, Type::Error);
     }
 
-    fn check_args(
-        &self,
-        function_type: FunctionType,
-        arguments: &[Type],
-        types: &TypeResolver,
-    ) -> Type {
-        if arguments.len() > function_type.parameters.len() {
+    let function_type = function_type.unwrap();
+    if arguments.len() > function_type.parameters.len() {
+        println!(
+            "Type error: Call expected at most {} arguments but recieved {}",
+            function_type.parameters.len(),
+            arguments.len()
+        );
+    }
+
+    let parameters_and_arguments = function_type.parameters.iter().zip(&arguments);
+    for (index, (parameter, argument)) in parameters_and_arguments.enumerate() {
+        if !argument.is_assignable_to(parameter, &scope.types) {
             println!(
-                "Type error: Call expected at most {} arguments but recieved {}",
-                function_type.parameters.len(),
-                arguments.len()
+                "Type error: Arg {} of type `{}` cannot be assigned to parameter of type `{}`",
+                index,
+                argument.format(&scope.types),
+                parameter.format(&scope.types),
             );
         }
+    }
 
-        for (index, (parameter, argument)) in
-            function_type.parameters.iter().zip(arguments).enumerate()
-        {
-            if !argument.is_assignable_to(parameter, types) {
-                println!(
-                    "Type error: Arg {} of type `{}` cannot be assigned to parameter of type `{}`",
-                    index,
-                    argument.format(types),
-                    parameter.format(types),
-                );
-            }
-        }
-
-        if arguments.len() < function_type.parameters.len() {
-            let remaining_parameters = &function_type.parameters[arguments.len()..];
-            Type::Function(FunctionType {
-                parameters: remaining_parameters.to_vec(),
-                return_type: function_type.return_type,
-            })
-        } else {
-            *function_type.return_type
-        }
+    if arguments.len() < function_type.parameters.len() {
+        let remaining_parameters = &function_type.parameters[arguments.len()..];
+        let result_type = Type::Function(FunctionType {
+            parameters: remaining_parameters.to_vec(),
+            return_type: function_type.return_type.clone(),
+        });
+        (scope, result_type)
+    } else {
+        (scope, *function_type.return_type.clone())
     }
 }
