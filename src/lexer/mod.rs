9e80d8c3e8491b5b6b0b5fe53a9ebe2359cd28e5
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-27 18:15:22 -0500

    More renames and moves

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,5 +1,7 @@
 mod character_span;
+mod keyword;
 mod source_code;
+mod symbol;
 mod token;
 mod tokenize;
 mod tokenizers;
@@ -7,6 +9,8 @@ mod tokenizers;
 use tokenize::*;
 
 pub use character_span::*;
+pub use keyword::*;
 pub use source_code::*;
+pub use symbol::*;
 pub use token::*;
 pub use tokenizers::*;
