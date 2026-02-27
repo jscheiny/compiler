7cd639eb18ddb88c62c6412869ae07404dc116ba
Jonah Scheinerman <jonah@scheinerman.net>
2026-02-27 18:07:29 -0500

    Rename directory

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -2,11 +2,11 @@ mod character_span;
 mod source_code;
 mod token;
 mod tokenizer;
-mod tokens;
+mod tokenizers;
 
 use tokenizer::*;
 
 pub use character_span::*;
 pub use source_code::*;
 pub use token::*;
-pub use tokens::*;
+pub use tokenizers::*;
