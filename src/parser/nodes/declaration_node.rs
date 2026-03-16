f522e0101045f1685b480ab64a9bcee9daa5ebef
Jonah Scheinerman <jonah@scheinerman.net>
2026-03-16 15:59:51 -0400

    Unnesting

--- src/parser/nodes/declaration_node.rs
+++ src/parser/nodes/declaration_node.rs
@@ -18,27 +18,36 @@ impl DeclarationNode {
             .map(|type_def| type_def.get_type(&scope, None));
 
         let (mut scope, resolved_type) = match self.initializer.as_ref() {
-            Some(initializer) => {
-                let (new_scope, resolved_type) =
-                    initializer.check_expected(scope, expected_type.as_ref());
-                if let Some(expected_type) = expected_type.as_ref() {
-                    if !resolved_type.is_assignable_to(expected_type, &new_scope) {
-                        new_scope.source.print_error(
-                            initializer.span,
-                            &format!(
-                                "Initializer not assignable to type `{}`",
-                                expected_type.format(&new_scope)
-                            ),
-                            &format!("found type: `{}`", resolved_type.format(&new_scope)),
-                        );
-                    }
-                }
-                (new_scope, expected_type.unwrap_or(resolved_type))
-            }
+            Some(initializer) => self.check_initializer(scope, expected_type, initializer),
             None => (scope, Type::Error),
         };
 
         scope.add_value(&self.name, resolved_type);
         scope
+    }
+
+    fn check_initializer(
+        &self,
+        scope: Box<Scope>,
+        expected_type: Option<Type>,
+        initializer: &Node<ExpressionNode>,
+    ) -> (Box<Scope>, Type) {
+        let (scope, resolved_type) = initializer.check_expected(scope, expected_type.as_ref());
+        let Some(expected_type) = expected_type else {
+            return (scope, expected_type.unwrap_or(resolved_type));
+        };
+
+        if !resolved_type.is_assignable_to(&expected_type, &scope) {
+            scope.source.print_error(
+                initializer.span,
+                &format!(
+                    "Initializer not assignable to type `{}`",
+                    expected_type.format(&scope)
+                ),
+                &format!("found type: `{}`", resolved_type.format(&scope)),
+            );
+        }
+
+        (scope, expected_type)
     }
 }
