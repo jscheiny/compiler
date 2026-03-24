46a39c321ac6a49c156f689dbfbe2b6e778d1da1
Jonah Scheinerman <jonah@scheinerman.net>
2026-03-24 00:45:26 -0400

    move parameter node

--- src/parser/nodes/functions/mod.rs
+++ src/parser/nodes/functions/mod.rs
@@ -2,8 +2,10 @@ mod function_body_node;
 mod function_node;
 mod function_signature_node;
 mod method_node;
+mod parameter_node;
 
 pub use function_body_node::*;
 pub use function_node::*;
 pub use function_signature_node::*;
 pub use method_node::*;
+pub use parameter_node::*;
