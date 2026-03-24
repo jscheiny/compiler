742f3a7266a7f3bab0c6ca4e5c403cac525099ca
Jonah Scheinerman <jonah@scheinerman.net>
2026-03-24 00:17:11 -0400

    Move operators

---
+++ src/parser/nodes/operators/mod.rs
@@ -0,0 +1,9 @@
+mod binary_operator;
+mod operator;
+mod postfix_operator;
+mod prefix_operator;
+
+pub use binary_operator::*;
+pub use operator::*;
+pub use postfix_operator::*;
+pub use prefix_operator::*;
