312c6899b3d6e23d7041b98a4340f219b5e09e5c
Jonah Scheinerman <jonah@scheinerman.net>
2026-03-24 00:09:44 -0400

    Move statement nodes into directory

---
+++ src/parser/nodes/statements/mod.rs
@@ -0,0 +1,19 @@
+mod block_node;
+mod declaration_node;
+mod if_statement_condition_node;
+mod if_statement_node;
+mod match_case_node;
+mod match_node;
+mod match_pattern_node;
+mod statement_node;
+mod while_loop_node;
+
+pub use block_node::*;
+pub use declaration_node::*;
+pub use if_statement_condition_node::*;
+pub use if_statement_node::*;
+pub use match_case_node::*;
+pub use match_node::*;
+pub use match_pattern_node::*;
+pub use statement_node::*;
+pub use while_loop_node::*;
