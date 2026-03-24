defba88c27d51f7ebebe35bf5b753abc00580ac2
Jonah Scheinerman <jonah@scheinerman.net>
2026-03-24 00:24:28 -0400

    Left over types

--- src/parser/nodes/types/mod.rs
+++ src/parser/nodes/types/mod.rs
@@ -1,3 +1,5 @@
+mod function_type_node;
+mod primitive_type;
 mod tuple_type_node;
 mod type_alias_node;
 mod type_node;
@@ -5,6 +7,8 @@ mod type_parameter_list_node;
 mod type_parameter_node;
 mod user_defined_type_node;
 
+pub use function_type_node::*;
+pub use primitive_type::*;
 pub use tuple_type_node::*;
 pub use type_alias_node::*;
 pub use type_node::*;
