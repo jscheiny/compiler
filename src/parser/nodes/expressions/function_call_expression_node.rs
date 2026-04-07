49654e0dcb8f52afddbe38adb2d1a71d2bc9f01f
Jonah Scheinerman <jonah@scheinerman.net>
2026-04-07 01:09:43 -0400

    Spreads in function calls

--- src/parser/nodes/expressions/function_call_expression_node.rs
+++ src/parser/nodes/expressions/function_call_expression_node.rs
@@ -1,8 +1,8 @@
-use std::rc::Rc;
+use std::{cmp::min, rc::Rc};
 
 use crate::{
     checker::{FunctionType, Scope, Type},
-    parser::{ExpressionNode, Node, NodeVec, TokenSpan},
+    parser::{ExpressionNode, Node, NodeVec, SpreadNode, TokenSpan},
 };
 
 pub struct FunctionCallExpressionNode {
@@ -49,7 +49,11 @@ fn check_invalid_function_call(
 
     // Check parameter types without expected argument types
     for argument in arguments.iter() {
-        scope = argument.check(scope).0;
+        if let ExpressionNode::Spread(spread_argument) = &argument.value {
+            scope = spread_argument.check_valid(scope, None).0;
+        } else {
+            scope = argument.check(scope).0;
+        }
     }
 
     (scope, Type::Error)
@@ -60,7 +64,41 @@ fn check_valid_function_call(
     function_type: &Rc<FunctionType>,
     arguments: &NodeVec<ExpressionNode>,
 ) -> (Box<Scope>, Type) {
-    if arguments.len() > function_type.parameters.len() {
+    let mut argument_count = 0;
+    for argument in arguments.iter() {
+        if let ExpressionNode::Spread(spread_node) = &argument.value {
+            (scope, argument_count) = check_spread_arg(
+                scope,
+                spread_node,
+                argument.span,
+                function_type,
+                argument_count,
+            );
+        } else {
+            let parameter_type = function_type.parameters.get(argument_count);
+            let (new_scope, argument_type) = argument.check_expected(scope, parameter_type);
+            scope = new_scope;
+
+            let Some(parameter_type) = parameter_type else {
+                continue;
+            };
+
+            if !argument_type.is_assignable_to(parameter_type, &scope) {
+                scope.source.print_error(
+                    argument.span,
+                    "Argument not assignable to parameter type",
+                    &format!(
+                        "expected type `{}`, found type `{}`",
+                        parameter_type.format(&scope),
+                        argument_type.format(&scope),
+                    ),
+                );
+            }
+            argument_count += 1;
+        }
+    }
+
+    if argument_count > function_type.parameters.len() {
         scope.source.print_error(
             arguments.span,
             "Too many arguments",
@@ -72,35 +110,13 @@ fn check_valid_function_call(
                 } else {
                     "s"
                 },
-                arguments.len()
+                argument_count
             ),
         );
     }
 
-    for (index, argument) in arguments.iter().enumerate() {
-        let parameter_type = function_type.parameters.get(index);
-        let (new_scope, argument_type) = argument.check_expected(scope, parameter_type);
-        scope = new_scope;
-
-        let Some(parameter_type) = parameter_type else {
-            continue;
-        };
-
-        if !argument_type.is_assignable_to(parameter_type, &scope) {
-            scope.source.print_error(
-                arguments[index].span,
-                "Argument not assignable to parameter type",
-                &format!(
-                    "expected type `{}`, found type `{}`",
-                    parameter_type.format(&scope),
-                    argument_type.format(&scope),
-                ),
-            );
-        }
-    }
-
-    if arguments.len() < function_type.parameters.len() {
-        let remaining_parameters = &function_type.parameters[arguments.len()..];
+    if argument_count < function_type.parameters.len() {
+        let remaining_parameters = &function_type.parameters[argument_count..];
         let result_type = Type::Function(FunctionType::new(
             remaining_parameters.to_vec(),
             *function_type.return_type.clone(),
@@ -109,4 +125,52 @@ fn check_valid_function_call(
     } else {
         (scope, *function_type.return_type.clone())
     }
+}
+
+fn check_spread_arg(
+    scope: Box<Scope>,
+    node: &SpreadNode,
+    span: TokenSpan,
+    function_type: &Rc<FunctionType>,
+    parameter_index: usize,
+) -> (Box<Scope>, usize) {
+    let expected_types = function_type
+        .parameters
+        .iter()
+        .skip(parameter_index)
+        .cloned()
+        .collect::<Vec<_>>();
+    let expected_type = Some(Type::Tuple(Rc::new(expected_types)));
+
+    let (scope, spread_type) = node.check_valid(scope, expected_type.as_ref());
+    for (offset, argument_type) in spread_type.iter().enumerate() {
+        let parameter_type = function_type.parameters.get(parameter_index + offset);
+
+        let Some(parameter_type) = parameter_type else {
+            break;
+        };
+
+        if !argument_type.is_assignable_to(parameter_type, &scope) {
+            let end_index = min(
+                parameter_index + spread_type.len(),
+                function_type.parameters.len(),
+            );
+            let expected_type = Type::Tuple(Rc::new(
+                function_type.parameters[parameter_index..end_index].to_vec(),
+            ));
+
+            scope.source.print_error(
+                span,
+                "Spread argument not assignable to parameter types",
+                &format!(
+                    "expected type `{}`, found type `{}`",
+                    expected_type.format(&scope),
+                    Type::Tuple(spread_type.clone()).format(&scope),
+                ),
+            );
+            break;
+        }
+    }
+
+    (scope, parameter_index + spread_type.len())
 }
